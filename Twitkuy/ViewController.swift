7e9eda7a95cca85a9934529b9c8d3d8be6bf64ba
Willa <willasaskara@gmail.com>
2019-09-30 23:59:19 +0700

    Initial Commit

---
+++ Twitkuy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twitkuy
+//
+//  Created by Willa on 30/09/19.
+//  Copyright © 2019 WillaSaskara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TwitkuyTests/TwitkuyTests.swift
@@ -0,0 +1,34 @@
+//
+//  TwitkuyTests.swift
+//  TwitkuyTests
+//
+//  Created by Willa on 30/09/19.
+//  Copyright © 2019 WillaSaskara. All rights reserved.
+//
+
+import XCTest
+@testable import Twitkuy
+
+class TwitkuyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
